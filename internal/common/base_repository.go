ba042ee62fb5e4ac91b918b39056c8fa7d103d19
Samrat Alam <samratalamshanto710@gmail.com>
2025-10-04 09:00:57 +0600

    Refactor: UserRepository, BaseRepository
    added interface based implementation

--- internal/common/base_repository.go
+++ internal/common/base_repository.go
@@ -4,16 +4,41 @@ import (
 	"gorm.io/gorm"
 )
 
-type BaseRepository[T any] struct {
-	db *gorm.DB
+// Define interface
+type BaseRepositoryInterface[T any] interface {
+	Save(entity *T) (*T, error)
+	FindById(id uint) (*T, error)
+	FindByIdList(idList []uint) ([]T, error)
+	FindAll() ([]T, error)
+	FindAllPagination(offset, limit int) ([]T, error)
+	DeleteById(id uint) (bool, error)
+	DeleteIdList(idList []uint) (bool, error)
 }
 
-func GetRepository[T any]() *BaseRepository[T] {
-	return &BaseRepository[T]{db: DBInstance}
+/*
+var _ Interface = &Struct{} -->compile-time assertion that *Struct implements the interface Interface.
+doesn’t actually create a variable at runtime — the blank identifier _ discards it.
+If Struct does not implement all methods of Interface, the compiler will throw an error.
+
+var x struct{}    // declare variable of type empty struct, default values assigned.
+y := struct{}{}   // create a new empty struct instance, zero bytes.const
+
+don’t care about the actual value — we only care that *Struct can be assigned to Interface.
+The variable name _ discards it (so no runtime memory is really kept).
+*/
+
+var _ BaseRepositoryInterface[any] = &BaseRepository[any]{}
+
+type BaseRepository[T any] struct {
+	DB *gorm.DB
+}
+
+func GetBaseRepository[T any](db *gorm.DB) *BaseRepository[T] {
+	return &BaseRepository[T]{DB: db}
 }
 
 func (r *BaseRepository[T]) Save(entity *T) (*T, error) {
-	if err := r.db.Save(entity).Error; err != nil {
+	if err := r.DB.Save(entity).Error; err != nil {
 		return nil, err
 	}
 	return entity, nil
@@ -21,21 +46,20 @@ func (r *BaseRepository[T]) Save(entity *T) (*T, error) {
 
 func (r *BaseRepository[T]) FindById(id uint) (*T, error) {
 	/*
-		var entity *T means: You created a variable of type *T
+		wrong: var entity *T means: You created a variable of type *T
 		Its default value is nil. So entity == nil
 
 		GORM tries to write the DB row into entity.
-		But entity is nil,
-		so GORM has no memory to put values into
+		But entity is nil, so GORM has no memory to put values into
 		→ it tries to dereference a nil pointer → panic.
 
-		You need a valid memory location for GORM to fill with DB data.
+		Right: You need a valid memory location for GORM to fill with DB data.
 		var entity T → creates a real T value on the stack (not nil).
 		&entity → gives GORM a pointer to write into.
 	*/
 
 	var entity T
-	if err := r.db.Take(&entity, id).Error; err != nil {
+	if err := r.DB.Take(&entity, id).Error; err != nil {
 		return nil, err
 	}
 	return &entity, nil
@@ -47,16 +71,16 @@ func (r *BaseRepository[T]) FindByIdList(idList []uint) ([]T, error) {
 		GORM inspects the type of T ([]T → T).Table name (users),Fields (ID, Name..)
 	*/
 
-	var resutlList []T
-	if err := r.db.Take(&resutlList, idList).Error; err != nil {
+	var resultList []T
+	if err := r.DB.Find(&resultList, idList).Error; err != nil {
 		return nil, err
 	}
-	return resutlList, nil
+	return resultList, nil
 }
 
 func (r *BaseRepository[T]) FindAll() ([]T, error) {
 	var resultList []T
-	if err := r.db.Find(&resultList).Error; err != nil {
+	if err := r.DB.Find(&resultList).Error; err != nil {
 		return nil, err
 	}
 	return resultList, nil
@@ -64,7 +88,7 @@ func (r *BaseRepository[T]) FindAll() ([]T, error) {
 
 func (r *BaseRepository[T]) FindAllPagination(offset, limit int) ([]T, error) {
 	var resultList []T
-	err := r.db.Limit(limit).Offset(offset).Find(&resultList).Error
+	err := r.DB.Limit(limit).Offset(offset).Find(&resultList).Error
 	if err != nil {
 		return nil, err
 	}
@@ -73,7 +97,7 @@ func (r *BaseRepository[T]) FindAllPagination(offset, limit int) ([]T, error) {
 
 func (r *BaseRepository[T]) DeleteById(id uint) (bool, error) {
 	var entity T //GORM need model reference to determines table name,primary key
-	res := r.db.Delete(&entity, id)
+	res := r.DB.Delete(&entity, id)
 	if err := res.Error; err != nil {
 		return false, err
 	}
@@ -82,7 +106,7 @@ func (r *BaseRepository[T]) DeleteById(id uint) (bool, error) {
 
 func (r *BaseRepository[T]) DeleteIdList(idList []uint) (bool, error) {
 	var entity T //GORM need model reference to determines table name,primary key
-	if err := r.db.Delete(&entity, idList).Error; err != nil {
+	if err := r.DB.Delete(&entity, idList).Error; err != nil {
 		return false, err
 	}
 	return true, nil
--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,59 +1,30 @@
 package user
 
-import "gorm.io/gorm"
+import (
+	"ecom-backend/internal/common"
 
-type IUserRepository interface {
-	Save(user *User) (*User, error)
-	FindByAccountId(accountId string) (*User, error)
-	FindById(id uint) (*User, error)
-	FindAll(limit, offset int) ([]User, error)
-	DeleteById(id uint) (bool, error)
+	"gorm.io/gorm"
+)
+
+type UserRepositoryInterface interface {
+	FindByAccountID(accountID string) (*User, error)
 }
 
-var _ IUserRepository = &UserRepository{}
+var _ UserRepositoryInterface = &UserRepository{}
 
 type UserRepository struct {
-	db *gorm.DB
+	*common.BaseRepository[User]
 }
 
-func GetRepository(db *gorm.DB) *UserRepository {
-	return &UserRepository{db}
+func GetUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{common.GetBaseRepository[User](db)}
 }
 
-func (r *UserRepository) Save(user *User) (*User, error) {
-	if err := r.db.Save(user).Error; err != nil {
-		return nil, err
-	}
-	return user, nil
-}
-
-func (r *UserRepository) DeleteById(id uint) (bool, error) {
-	if err := r.db.Delete(&User{}, id).Error; err != nil {
-		return false, err
-	}
-	return true, nil
-}
-
-func (r *UserRepository) FindByAccountId(accountId string) (*User, error) {
+func (r *UserRepository) FindByAccountID(accountID string) (*User, error) {
 	var user User
-	if err := r.db.Where("account_id=?", accountId).First(&user).Error; err != nil {
+	err := r.BaseRepository.DB.Where("account_id=?", accountID).Take(&user).Error
+	if err != nil {
 		return nil, err
 	}
 	return &user, nil
-}
-
-func (r *UserRepository) FindById(id uint) (*User, error) {
-	var user User
-	if err := r.db.First(&user, id).Error; err != nil {
-		return nil, err
-	}
-	return &user, nil
-}
-
-func (r *UserRepository) FindAll(limit, offset int) ([]User, error) {
-	var list []User
-	if err := r.db.Limit(limit).Offset(offset).Find(&list).Error; err != nil {
-		return nil, err
-	}
-	return list, nil
 }
